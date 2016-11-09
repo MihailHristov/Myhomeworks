94f1bbf1cbf224607272d6aacafe2057fb6a9524
MihailHristov <mihail.hr7@gmail.com>
2016-11-09 05:07:59 +0200

    Add files via upload

---
+++ Birds.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Birds
+{
+    class Birds
+    {
+        static void Main(string[] args)
+        {
+            double birds = double.Parse(Console.ReadLine());
+            double feathers = double.Parse(Console.ReadLine());
+            double result = 0;
+            double average = (feathers / (double)birds);
+            if (birds==0&&feathers==0)
+            {
+                Console.WriteLine(result);
+            }
+            else if (birds%2==0)
+            {
+                result = average * 123123123123;
+            }
+            else
+            {
+                result =average/317;
+            }
+            Console.WriteLine("{0:f4}",result);
+        }
+    }
+}
